81c3b6ef496e8e8fe08e0f90efe1376d3db11f87
florianwoelki <florianwoelki@gmx.de>
2020-05-13 11:53:32 +0200

    build: prioritize default file

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ const project = require('./package.json');
 const head = '\/*\r\n* YACSS ' + project.version + `\r\n* Florian Woelki, Copyright ${(new Date()).getFullYear()}\r\n* http://florianwoelki.github.io/YACSS\r\n*/\r\n`;
 
 gulp.task('compile', () => {
-  return gulp.src('src/**/*.scss')
+  return gulp.src(['src/default.scss', 'src/**/*.scss'])
     .pipe(sass.sync().on('error', sass.logError))
     .pipe($.header(head))
     .pipe($.size())
