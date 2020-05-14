eab7cd3f04168840a57e77fce43e1277f0e9e3c6
florianwoelki <florianwoelki@gmx.de>
2020-05-14 11:35:59 +0200

    feat: add customizable primary and accent colors

--- dist/yacss.js
+++ dist/yacss.js
@@ -3,3 +3,40 @@
 * Florian Woelki, Copyright 2020
 * http://florianwoelki.github.io/YACSS
 */
+var colors = {
+    blue: {
+        '100': '#ebf8ff',
+        '200': '#bee3f8',
+        '300': '#90cdf4',
+        '400': '#63b3ed',
+        '500': '#4299e1',
+        '600': '#3182ce',
+        '700': '#2b6cb0',
+        '800': '#2c5282',
+        '900': '#2a4365'
+    },
+    red: {
+        '100': '#fff5f5',
+        '200': '#fed7d7',
+        '300': '#feb2b2',
+        '400': '#fc8181',
+        '500': '#fc8181',
+        '600': '#e53e3e',
+        '700': '#c53030',
+        '800': '#9b2c2c',
+        '900': '#742a2a'
+    },
+};
+function initTheme(theme) {
+    var root = document.documentElement;
+    if (root) {
+        if (theme.primaryColor in colors && theme.accentColor in colors) {
+            var primaryColors = colors[theme.primaryColor];
+            var accentColors = colors[theme.accentColor];
+            for (var i = 100; i <= 900; i += 100) {
+                root.style.setProperty("--primary-color-" + i, primaryColors[i]);
+                root.style.setProperty("--accent-color-" + i, accentColors[i]);
+            }
+        }
+    }
+}
