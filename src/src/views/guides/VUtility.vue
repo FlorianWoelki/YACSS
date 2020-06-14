5357e1eea49b47aa74f79ebe68f51a8a63d26f26
florianwoelki <florianwoelki@gmx.de>
2020-06-14 19:16:09 +0200

    feat(view): add all color classes

--- src/src/views/guides/VUtility.vue
+++ src/src/views/guides/VUtility.vue
@@ -22,8 +22,40 @@
         </template>
         <slot>
           <tr>
-            <td>.text-blue-{100-900}</td>
-            <td>Colorize the text with blue (see color range)</td>
+            <td>.text-white</td>
+            <td>Colorize the text white</td>
+          </tr>
+          <tr>
+            <td>.text-hover-white</td>
+            <td>Colorize the text white on hover</td>
+          </tr>
+          <tr>
+            <td>.text-black</td>
+            <td>Colorize the text black</td>
+          </tr>
+          <tr>
+            <td>.text-hover-black</td>
+            <td>Colorize the text black on hover</td>
+          </tr>
+          <tr v-for="color in colors" :key="color">
+            <td>.text-{{ color }}-{100-900}</td>
+            <td>Colorize the text with {{ color }} (see color range)</td>
+          </tr>
+          <tr v-for="color in colors" :key="color">
+            <td>.bg-{{ color }}-{100-900}</td>
+            <td>Colorize the background with {{ color }} (see color range)</td>
+          </tr>
+          <tr v-for="color in colors" :key="color">
+            <td>.bc-{{ color }}-{100-900}</td>
+            <td>Colorize the border color with {{ color }} (see color range)</td>
+          </tr>
+          <tr v-for="color in colors" :key="color">
+            <td>.text-hover-{{ color }}-{100-900}</td>
+            <td>Colorize the text color with {{ color }} on hover (see color range)</td>
+          </tr>
+          <tr v-for="color in colors" :key="color">
+            <td>.bg-hover-{{ color }}-{100-900}</td>
+            <td>Colorize the background color with {{ color }} on hover (see color range)</td>
           </tr>
         </slot>
       </UtilityTable>
@@ -39,6 +71,11 @@ export default {
   components: {
     Navbar,
     UtilityTable,
+  },
+  data() {
+    return {
+      colors: ['blue', 'gray', 'purple', 'indigo', 'red', 'green'],
+    };
   },
 };
 </script>
