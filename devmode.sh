2048bbe7965313761fee3921279ffca7d4f82458
FlorianWoelki <FlorianWoelki@gmx.de>
2020-06-22 14:44:32 +0200

    build: remove lock file and change dev script

--- devmode.sh
+++ devmode.sh
@@ -3,26 +3,26 @@
 cat > index.html << ENDOFFILE
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 
-  <link rel="stylesheet" href="./dist/yacss.min.css">
-</head>
-<body>
-  <div class="container">
-    <h1>Hello Devmode</h1>
-    <button>Awesome Button</button>
-  </div>
-  <script src="./dist/yacss.min.js"></script>
-  <script>
-    initTheme({
-      primaryColor: 'purple',
-      accentColor: 'red',
-      classes: ['all'],
-    });
-  </script>
-</body>
+    <link rel="stylesheet" href="./dist/yacss.min.css">
+  </head>
+  <body>
+    <div class="container">
+      <h1>Hello Devmode</h1>
+      <button>Awesome Button</button>
+    </div>
+    <script src="./dist/yacss.min.js"></script>
+    <script>
+      initTheme({
+        primaryColor: 'purple',
+        accentColor: 'red',
+        classes: ['all'],
+      });
+    </script>
+  </body>
 </html>
 ENDOFFILE
