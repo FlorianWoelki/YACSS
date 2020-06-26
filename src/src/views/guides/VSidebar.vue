a192bd8777831a528b32b1f489f1eeb2a2624443
florianwoelki <florianwoelki@gmx.de>
2020-06-26 15:32:48 +0200

    feat(view): add missing views

---
+++ src/src/views/guides/VSidebar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="sidebar-section">
+    <ComingSoon />
+  </div>
+</template>
+
+<script>
+import ComingSoon from '@/components/ComingSoon.vue';
+
+export default {
+  components: {
+    ComingSoon,
+  },
+};
+</script>
