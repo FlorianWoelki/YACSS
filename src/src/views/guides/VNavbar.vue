18aa4d69dc494fcc58296febef6f6503d3448166
florianwoelki <florianwoelki@gmx.de>
2020-06-26 15:29:15 +0200

    feat(views): add Navbar view

---
+++ src/src/views/guides/VNavbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="navbar">
+    <ComingSoon />
+  </div>
+</template>
+
+<script>
+import ComingSoon from '@/components/ComingSoon.vue';
+
+export default {
+  components: {
+    ComingSoon,
+  },
+};
+</script>
