be63dd68dc41c74b4dcfd86b0c072003f05e320f
florianwoelki <florianwoelki@gmx.de>
2020-06-26 15:28:45 +0200

    feat(components): add ComingSoon component

---
+++ src/src/components/ComingSoon.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="coming-soon">
+    <div class="container mt-4 mb-48">
+      <h1>Coming Soon! We are working on it!</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ComingSoon',
+};
+</script>
